b9d699b97fdc702a11cf7a9dec25d3760abe3772
Anuj5588 <ak4826636@gmail.com>
2023-05-31 12:19:23 +0530

    Updates

---
+++ precache-manifest.8d7e1c48dd61e8dbdefffed70a5ce9bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f284d74baec4c143f5e8221258b9b281",
+    "url": "/analytic/index.html"
+  },
+  {
+    "revision": "fe16f027b02647805a65",
+    "url": "/analytic/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "0c13b18a502c61870664",
+    "url": "/analytic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe16f027b02647805a65",
+    "url": "/analytic/static/js/2.32785ecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/analytic/static/js/2.32785ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c13b18a502c61870664",
+    "url": "/analytic/static/js/main.73e4b763.chunk.js"
+  },
+  {
+    "revision": "a1b06e453c5d8a67b725",
+    "url": "/analytic/static/js/runtime-main.ec92053a.js"
+  }
+]);
